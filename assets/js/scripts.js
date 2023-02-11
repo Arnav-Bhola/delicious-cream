a1f88a4c4da5d56a13cc21a5e530a53bc34c9477
arnav <arnav.bholausa@gmail.com>
2023-02-11 16:12:12 -0400

    Navbar toggler fix 1

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,8 @@
 const toggleBtn = document.querySelector("#toggle-nav");
-const navbar = document.querySelector(".navbar-links")
+const navbar = document.querySelector(".navbar");
+const navbarLinks = document.querySelector(".navbar-links");
 
 toggleBtn.addEventListener("click", function () {
-    navbar.classList.toggle("navbar-show")
-})
+    navbar.classList.toggle("navbar-show");
+    navbarLinks.classList.toggle("navbar-links-show");
+});
