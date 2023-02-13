4256f4330713b345eab10c1383fce92e84a10da7
arnav <arnav.bholausa@gmail.com>
2023-02-12 21:13:33 -0400

    Added html structure for carousel

--- _site/assets/js/scripts.js
+++ _site/assets/js/scripts.js
@@ -6,3 +6,10 @@ toggleBtn.on("click", function () {
     navbar.toggleClass("navbar-show");
     navbarLinks.toggleClass("navbar-links-show");
 });
+
+const mostLovedCarousel = $('.most-loved.carousel')
+var mostLoved = new Flickity( carousel, {
+    cellAlign: 'left',
+    wrapAround: true,
+    freeScroll: true,
+})
