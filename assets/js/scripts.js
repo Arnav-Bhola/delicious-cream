71a09d512c1ed4b281a6c602ddb1cd5a1a786cff
arnav <arnav.bholausa@gmail.com>
2023-02-13 16:14:06 -0400

    Adding hero section

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,3 +12,26 @@ var mostLovedCarousel = document.querySelector('.most-loved-carousel');
     cellAlign: 'left',
     contain: true
   });
+
+  window.addEventListener("scroll", scrollAnimation);
+
+
+const imgContainer = document.querySelector('.heroImage').style
+const initialWidth = imgContainer.width
+
+function scrollAnimation() {
+    const content = document.querySelector(".heroImage").getBoundingClientRect()
+    const initialHeight = content.height;
+    const finalHeight = initialHeight * 0.7;
+    const factor = (initialHeight - finalHeight) / 30;
+    let result = content.bottom / factor;
+
+    if (result > 100) {
+        result = 100
+    }
+    if (result < 60) {
+        result = 60
+    }
+    console.log(result);
+    imgContainer.width = result + '%'
+}
