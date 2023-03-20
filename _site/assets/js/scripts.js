3092313b92ceae259f19bd61b56e89445db884a2
arnav <arnav.bholausa@gmail.com>
2023-03-20 16:52:06 -0300

    Fixed some stuff

--- _site/assets/js/scripts.js
+++ _site/assets/js/scripts.js
@@ -4,95 +4,95 @@ const navbar = $(".navbar");
 const navbarLinks = $(".navbar-links");
 
 toggleBtn.on("click", function () {
-    navbar.toggleClass("navbar-show");
-    navbarLinks.toggleClass("navbar-links-show");
+  navbar.toggleClass("navbar-show");
+  navbarLinks.toggleClass("navbar-links-show");
 });
 
 // ***** CAROUSEL *****
-var mostLovedCarousel = document.querySelector('.most-loved-carousel');
-  var flkty = new Flickity( mostLovedCarousel, {
-    // options
-    draggable: '>1',
-    freeScroll: true,
-    contain: true,
-    wrapAround: true,
-    cellAlign: 'left',
-    autoPlay: true,
-    autoPlay: 5000,
-    pauseAutoPlayOnHover: true,
-    fullscreen: true,
-    dragThreshold: 10,
-    freeScroll: true,
-    freeScrollFriction: 0.03,
-    accessibility: true,
-    prevNextButtons: true,
-    pageDots: true,
-    
-    // arrowShape: {
-    //   x0: 10,
-    //   x1: 60,
-    //   y1: 45,
-    //   x2: 80,
-    //   y2: 45,
-    //   x3: 30,
-    // }
-  });
+var mostLovedCarousel = document.querySelector(".most-loved-carousel");
+var flkty = new Flickity(mostLovedCarousel, {
+  // options
+  draggable: ">1",
+  freeScroll: true,
+  contain: true,
+  wrapAround: true,
+  cellAlign: "left",
+  autoPlay: true,
+  autoPlay: 5000,
+  pauseAutoPlayOnHover: true,
+  fullscreen: true,
+  dragThreshold: 10,
+  freeScroll: true,
+  freeScrollFriction: 0.03,
+  accessibility: true,
+  prevNextButtons: true,
+  pageDots: true,
+
+  // arrowShape: {
+  //   x0: 10,
+  //   x1: 60,
+  //   y1: 45,
+  //   x2: 80,
+  //   y2: 45,
+  //   x3: 30,
+  // }
+});
 
 // ***** SCROLL ANIMATION *****
-const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+  navigator.userAgent
+);
 
-if(!isMobile) {
-    window.addEventListener("scroll", scrollAnimation);
+if (!isMobile) {
+  window.addEventListener("scroll", scrollAnimation);
 }
 
-const imgContainer = document.querySelector('.heroImage').style
-const initialWidth = imgContainer.width
+const imgContainer = document.querySelector(".heroImage").style;
+const initialWidth = imgContainer.width;
 
 function scrollAnimation() {
-    const content = document.querySelector(".heroImage").getBoundingClientRect()
-    const initialHeight = content.height;
-    const finalHeight = initialHeight * 0.7;
-    const factor = (initialHeight - finalHeight) / 30;
-    let result = content.bottom / factor;
+  const content = document.querySelector(".heroImage").getBoundingClientRect();
+  const initialHeight = content.height;
+  const finalHeight = initialHeight * 0.7;
+  const factor = (initialHeight - finalHeight) / 30;
+  let result = content.bottom / factor;
 
-    if (result > 100) {
-        result = 100
-    }
-    if (result < 60) {
-        result = 60
-    }
-    console.log(result);
-    imgContainer.width = result + '%'
+  if (result > 100) {
+    result = 100;
+  }
+  if (result < 60) {
+    result = 60;
+  }
+  console.log(result);
+  imgContainer.width = result + "%";
 }
 
-
 // ***** INTERSECTION OBSERVER *****
-const intersectionContainer = document.querySelectorAll('#delicious .flexContainer');
+const intersectionContainer = document.querySelectorAll("#delicious .flexContainer");
 
 const options = {
   root: null,
   threshold: 0.5,
-  rootMargin: '0px'
+  rootMargin: "0px",
 };
 
-const observer = new IntersectionObserver(items => {
-  items.forEach(item => {
+const observer = new IntersectionObserver((items) => {
+  items.forEach((item) => {
     if (item.isIntersecting) {
-      item.target.classList.add('show');
+      item.target.classList.add("show");
       observer.unobserve(item.target);
     }
   });
 }, options);
 
-intersectionContainer.forEach(item => {
+intersectionContainer.forEach((item) => {
   observer.observe(item);
 });
-
 
 // ***** PARTICLES / SPRINKLES *****
 // var particles_settings = "assets/js/particles.json"
 // var particles_duplicity = ['particles1', 'particles2', 'particles4', 'particles5']
-    
+
 // particles_duplicity.forEach(function(element) {
 //   console.log(element);
 //   particlesJS.load(element, particles_settings, function () {
@@ -100,6 +100,13 @@ intersectionContainer.forEach(item => {
 //   })
 // })
 
-window.addEventListener('scroll', () => {
-  document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-}, false);
+window.addEventListener(
+  "scroll",
+  () => {
+    document.body.style.setProperty(
+      "--scroll",
+      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+    );
+  },
+  false
+);
